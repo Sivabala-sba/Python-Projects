35f798f45f9ca5016ffbd686e3b8a9d0aba36f5e
Sivabala-sba <87258592+Sivabala-sba@users.noreply.github.com>
2021-11-10 22:39:32 +0530

    Add files via upload

---
+++ sumofarray.py
@@ -0,0 +1,24 @@
+#python 3 code to find sum of elements in given array
+# method 1
+"""def _sum(arr):
+    sum=0
+
+    for i in arr:
+        sum=sum+i
+    return(sum)
+arr=[]
+arr=[12, 3, 4, 15]
+
+n=len(arr)
+ans= _sum(arr)
+
+print('sum of the array is ', ans)"""
+
+# method 2
+arr=[]
+
+arr= [12, 3, 4, 15]
+
+ans= sum(arr)
+
+print ('sum of the array is ', ans)
